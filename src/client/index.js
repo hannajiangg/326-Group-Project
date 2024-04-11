1481ac49799b757980284c0960f83da48703ea35
Sean McGonigle <smcgonigle@umass.edu>
2024-04-11 18:14:50 -0400

    changed routing stuff

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,27 @@
 // Does routing and stuff
-async function loadLoginPage(){
-    const loginText = await fetch("./login.html").then(res => res.text());
-    document.body.innerHTML = loginText;
+const appState = {
+  currentView: "",
+};
+
+function loadView(view) {
+  fetch(`${view}.html`) // Assuming each view has a corresponding HTML file
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("view-container").innerHTML = html;
+      appState.currentView = view;
+      window.history.pushState({ view: view }, `${view}`, `#${view}`);
+    });
 }
-loadLoginPage();
+
+// Handle browser back and forward buttons
+window.addEventListener("popstate", (e) => {
+  if (e.state && e.state.view) {
+    loadView(e.state.view);
+  }
+});
+
+// Load the initial view based on the URL
+const initialView = window.location.hash
+  ? window.location.hash.substring(1)
+  : "home";
+loadView(initialView);
