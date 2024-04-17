1c9188ce732fe7e8a62bc4ef4b20a8d4c301776c
maxwell3025 <amoinus@gmail.com>
2024-04-17 10:31:12 -0400

    Added Better Navigation Layout

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,37 @@
+import * as login from "./login.js"
+import * as main from "./main.js"
+import * as product from "./product.js"
+import * as profile from "./profile.js"
+import * as register from "./register.js"
+import * as seller from "./seller.js"
+
 // Does routing and stuff
 const appState = {
   currentView: "",
 };
 
-function loadView(view) {
-  fetch(`${view}.html`) // Assuming each view has a corresponding HTML file
+async function loadView(view) {
+  await fetch(`${view}.html`) // Assuming each view has a corresponding HTML file
     .then((response) => response.text())
     .then((html) => {
       document.body.innerHTML = html;
       appState.currentView = view;
       window.history.pushState({ view: view }, `${view}`, `#${view}`);
     });
+  switch(view){
+    case "login":
+      // do stuff
+    case "main":
+      // do stuff
+    case "product":
+      // do stuff
+    case "profile":
+      // do stuff
+    case "register":
+      // do stuff
+    case "seller":
+      // do stuff
+  }
 }
 
 // Handle browser back and forward buttons
@@ -24,4 +45,5 @@ window.addEventListener("popstate", (e) => {
 const initialView = window.location.hash
   ? window.location.hash.substring(1)
   : "login";
+
 loadView(initialView);
