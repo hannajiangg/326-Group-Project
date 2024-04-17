b3f985b04e798e5f6c3c6c1d72f5ab20f4168f79
sagarikasonni <sagarikasonni24@gmail.com>
2024-04-17 14:55:07 -0400

    made a link to the login button

--- src/client/login.js
+++ src/client/login.js
@@ -2,3 +2,7 @@
 export function onNavigate(){
     console.log("Hello World!");
 }
+// let loginBtn = document.getElementById("loginBtn")
+// loginBtn.addEventListener("click", () => {
+//     window.location.href = "http://127.0.0.1:8080/mainPg.html"
+// })
