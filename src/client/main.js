5ffed7594e5893883c800343deed2005d306f8c8
maxwell3025 <amoinus@gmail.com>
2024-04-17 20:01:08 -0400

    Improved Layout

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,10 @@
+import { getListings } from "./api.js";
 import { loadView } from "./index.js";
 
-export function onNavigate(){
-    /** @type {HTMLInputElement} */
+export function onNavigate() {
+    /** @type {HTMLButtonElement} */
     const homeButtonElement = document.getElementById("home-button");
-    /** @type {HTMLInputElement} */
+    /** @type {HTMLButtonElement} */
     const sellButtonElement = document.getElementById("sell-button");
     /** @type {HTMLElement} */
     const userPortalElement = document.getElementById("user-portal");
@@ -11,4 +12,13 @@ export function onNavigate(){
     userPortalElement.addEventListener("click", () => loadView("main"));
     sellButtonElement.addEventListener("click", () => loadView("seller"));
     userPortalElement.addEventListener("click", () => loadView("profile"));
+    populateListings();
+}
+
+async function populateListings() {
+    const mainPageDisplay = document.getElementById("main-page-display");
+    (await getListings()).forEach(_id => {
+        
+
+    });
 }
