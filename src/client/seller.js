8e5f271451db276b3d10ec833fe7e476752349c8
maxwell3025 <amoinus@gmail.com>
2024-04-18 16:20:36 -0400

    Added New Image Thingy

--- src/client/seller.js
+++ src/client/seller.js
@@ -17,6 +17,13 @@ export async function onNavigate(){
         carouselImagesDiv.appendChild(carouselImage);
         carouselImageList.push(carouselImage)
     }
+    // Append placeholder for adding image
+    const newImagePlaceholderElement = document.createElement("div");
+    newImagePlaceholderElement.id = "new-image-placeholder";
+    newImagePlaceholderElement.innerHTML = 
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>`
+    carouselImagesDiv.appendChild(newImagePlaceholderElement);
+    carouselImageList.push(newImagePlaceholderElement);
     function jumpToImage(index){
         const imageWidthList = carouselImageList.map(image => image.getBoundingClientRect().width);
         const prevWidths = imageWidthList.slice(0, index);
