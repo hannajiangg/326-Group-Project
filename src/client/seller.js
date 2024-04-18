2db60cdb8761abd4e5b11ec5a789b73f2017f677
maxwell3025 <amoinus@gmail.com>
2024-04-18 16:31:37 -0400

    Fixed navigation bug

--- src/client/seller.js
+++ src/client/seller.js
@@ -1,6 +1,6 @@
 import { blobToURL, getListing } from "./api.js";
 
-export async function onNavigate(){
+export async function onNavigate() {
     const searchParams = new URLSearchParams(window.location.search);
     const listing = await getListing(searchParams.get("id"));
 
@@ -9,7 +9,7 @@ export async function onNavigate(){
     const carouselLeftArrow = document.getElementById("carousel-left-arrow");
     const carouselRightArrow = document.getElementById("carousel-right-arrow");
     const carouselImageList = [];
-    for(const imageBlob of listing.carousel){
+    for (const imageBlob of listing.carousel) {
         /** @type { HTMLImageElement } */
         const carouselImage = document.createElement("img");
         carouselImage.src = await blobToURL(imageBlob);
@@ -20,27 +20,31 @@ export async function onNavigate(){
     // Append placeholder for adding image
     const newImagePlaceholderElement = document.createElement("div");
     newImagePlaceholderElement.id = "new-image-placeholder";
-    newImagePlaceholderElement.innerHTML = 
-    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>`
+    newImagePlaceholderElement.innerHTML =
+        `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>`
     carouselImagesDiv.appendChild(newImagePlaceholderElement);
     carouselImageList.push(newImagePlaceholderElement);
-    function jumpToImage(index){
+    function jumpToImage(index) {
         const imageWidthList = carouselImageList.map(image => image.getBoundingClientRect().width);
         const prevWidths = imageWidthList.slice(0, index);
         let offset = -prevWidths.reduce((a, b) => a + b, 0);
         offset -= imageWidthList[index] * 0.5;
         const totalWidth = carouselDiv.getBoundingClientRect().width;
         offset += totalWidth * 0.5;
-        carouselImagesDiv.style.left = `${offset}px`
+        carouselImagesDiv.style.left = `${offset}px`;
     }
-    let currentImage = 0;
-    jumpToImage(currentImage);
-    carouselLeftArrow.addEventListener("click", ()=>{
+    let currentImage = carouselImageList.length - 1;
+    carouselImageList.forEach(element => element.addEventListener(
+        "load", () => {
+            jumpToImage(currentImage)
+        }
+    ))
+    carouselLeftArrow.addEventListener("click", () => {
         currentImage = Math.max(0, currentImage - 1);
         jumpToImage(currentImage)
     })
-    carouselRightArrow.addEventListener("click", ()=>{
-        currentImage = Math.min(carouselImageList.length, currentImage + 1);
+    carouselRightArrow.addEventListener("click", () => {
+        currentImage = Math.min(carouselImageList.length - 1, currentImage + 1);
         jumpToImage(currentImage)
     })
 }
