36b7ff7803595190856b67be6ec9da65bb0aa7d3
sagarikasonni <sagarikasonni24@gmail.com>
2024-04-18 11:54:27 -0400

    trying to do authentication

--- src/client/login.js
+++ src/client/login.js
@@ -1,8 +1,10 @@
 // This is example code. Feel free to delete.
-export function onNavigate(){
-    console.log("Hello World!");
-}
+// export function onNavigate(){
+//     console.log("Hello World!");
+// }
 // let loginBtn = document.getElementById("loginBtn")
 // loginBtn.addEventListener("click", () => {
 //     window.location.href = "http://127.0.0.1:8080/mainPg.html"
 // })
+let loginForm = document.getElementById('loginForm')
+let usernameInp = document.getElementById('password')
