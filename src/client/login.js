6a03c3de5edb2d891cf1bf82fcef57409c036ccf
sagarikasonni <sagarikasonni24@gmail.com>
2024-04-19 19:42:48 -0400

    working on authentication using postman

--- src/client/login.js
+++ src/client/login.js
@@ -1,10 +1,11 @@
-// This is example code. Feel free to delete.
-// export function onNavigate(){
-//     console.log("Hello World!");
-// }
-// let loginBtn = document.getElementById("loginBtn")
-// loginBtn.addEventListener("click", () => {
-//     window.location.href = "http://127.0.0.1:8080/mainPg.html"
+// let loginBtn = document.getElementById('loginBtn')
+// console.log(loginBtn)
+// loginBtn.addEventListener("submit", () => {
+//     // window.location.assign("http://127.0.0.1:8080/main.html")
+//     window.location.href = "http://127.0.0.1:8080/main.html"
 // })
-let loginForm = document.getElementById('loginForm')
-let usernameInp = document.getElementById('password')
+let loginForm = document.getElementById("loginForm")
+loginForm.addEventListener("submit", () => {
+    event.preventDefault()
+    window.location.href = "http://127.0.0.1:8080/main.html"
+})
