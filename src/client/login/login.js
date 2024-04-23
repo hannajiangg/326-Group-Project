c3695178c48d8db49302f64bbe89fa8818238844
maxwell3025 <amoinus@gmail.com>
2024-04-23 18:55:17 -0400

    Changed Login to use Google Login Chip

--- src/client/login/login.js
+++ src/client/login/login.js
@@ -1,12 +1,31 @@
-// This is example code. Feel free to delete.
-// export function onNavigate(){
-//     console.log("Hello World!");
-// }
-// let loginBtn = document.getElementById("loginBtn")
-// loginBtn.addEventListener("click", () => {
-//     window.location.href = "http://127.0.0.1:8080/mainPg.html"
-// })
-export function onNavigate(){
+async function googleSignIn(token) {
+    console.log(token);
+    const response = await fetch('./api/googleLogIn', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(token),
+    });
+    if(response.status !== 200){
+        console.log("Login Failed");
+    } else {
+        loadView('main');
+    }
+}
+
+export function onNavigate() {
+    google.accounts.id.initialize({
+        client_id: '681506738065-lm05vc09padgknbbk7i1kvbifshr00qj.apps.googleusercontent.com',
+        callback: googleSignIn,
+    });
+    google.accounts.id.renderButton(
+        document.getElementById('google-sign-in'),
+        {
+            type: 'standard',
+            shape: 'pill',
+            width: '200',
+        }
+    );
+
     let loginForm = document.getElementById('loginForm')
     let usernameInp = document.getElementById('password')
 }
