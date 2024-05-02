31b953d36a7780dc6831d375ec8b6717cff1c84d
maxwell3025 <amoinus@gmail.com>
2024-05-02 16:26:28 -0400

    Cleaned up server-side routing

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-// import logger from 'morgan'
+import logger from 'morgan'
 // import { loadView } from '../client/index.js'
 
 const app = express()
@@ -8,42 +8,8 @@ const port = 3000
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static('../src/client'))
+app.use(express.static('src/client'))
 
-// app.get('/login', async (req, res) => {
-//     await loadView('login', req.query)
-// })
-
-// app.get('/register', async (req, res) => {
-//     await loadView('register', req.query)
-// })
-
-// app.get('/main', async (req, res) => {
-//     await loadView('main', req.query)
-// })
-
-// app.get('/seller', async (req, res) => {
-//     await loadView('seller', req.query)
-// })
-
-// app.get('/product', async (req, res) => {
-//     await loadView('seller', req.query)
-// })
-
-// app.get('/profile', async (req, res) => {
-//     await loadView('seller', req.query)
-// })
-
-// // Handle unsupported methods for all routes
-// app.all('*', (req, res) => {
-//   res.status(405).send('Method Not Allowed')
-// })
-
-// // Handle unknown routes
-// app.all('*', (req, res) => {
-//   res.status(404).send('Not Found')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
