0b7fbb2caf1a66efe0b30369a03511eacd311db6
maxwell3025 <amoinus@gmail.com>
2024-05-02 16:33:22 -0400

    Fixed Bugs with Server Deployment

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,16 @@ import logger from 'morgan'
 // import { loadView } from '../client/index.js'
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('src/client'))
+
+app.post("/api/login", (req, res) => {
+  console.log("login tested");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
