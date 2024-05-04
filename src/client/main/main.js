6462662b8f93049ee10067b02a545e36392e4aa2
Sean McGonigle <smcgonigle@umass.edu>
2024-05-03 22:47:32 -0400

    Main page search bar is functional

--- src/client/main/main.js
+++ src/client/main/main.js
@@ -9,12 +9,12 @@ export function onNavigate() {
     /** @type {HTMLElement} */
     const userPortalElement = document.getElementById("user-portal");
     const searchBar = document.getElementById("search-bar");
+    const listings = [];
 
     homeButtonElement.addEventListener("click", () => loadView("main"));
     sellButtonElement.addEventListener("click", sellItem);
     userPortalElement.addEventListener("click", () => loadView("profile"));
     populateListings();
-    searchBar.addEventListener("input", search);
 
     /**
      * @param {Listing} listing 
@@ -48,13 +48,58 @@ export function onNavigate() {
         productBox.addEventListener("click", () => loadView("product", { id: listing._id }))
 
         mainPageDisplay.appendChild(productBox);
+        listings.push(productBox);
     }
 
     async function populateListings() {
         (await getListings()).forEach(async _id => {
             addListing(await getListing(_id))
         });
+        searchBar.addEventListener("input", search);
     }
 
-    function search() {}
+    // Only displays listings that contain every search term somewhere in the title or category
+    // Case insensitive, and ignores commas and apostrophes
+    // Updates on every user keypress, so no need for separate search button
+    function search() {
+        const terms = searchBar.value.toLowerCase().replaceAll(/[,']/g, "").split(" ");
+        listings.forEach(listing => {
+            const title = listing.querySelector("h3").innerText.toLowerCase().replaceAll(/[,']/g, "");
+            const category = listing.querySelector("p").innerText.toLowerCase().replaceAll(/[,']/g, "");
+            if (terms.every(term => title.includes(term) || category.includes(term))) {
+                listing.hidden = false;
+            } else {
+                listing.hidden = true;
+            }
+        })
+    }
+
+
+// Extra dummy items for testing search bar
+//
+// const items = [["Men's jeans", "Clothing"], ["Women's jeans, used", "Clothing"], ["Men's shirt", "Clothing"], ["Women's shirt", "Clothing"],
+//                 ["New bike", "Item"], ["Used pot", "Item"], ["New basketball", "Item, Sports"], ["Soccer cleats", "Clothing, Sports"],
+//                 ["Baseball bat", "Sports"]];
+// items.forEach(item => {
+//     const mainPageDisplay = document.getElementById("main-page-display");
+//     const productBox = document.createElement("div");
+//     productBox.classList.add("product-box");
+//     productBox.innerHTML = `
+//     <div class="image-container">
+//         <div class="price-tag"></div>
+//     </div>
+//     <div class="description-box">
+//         <h3></h3>
+//         <p></p>
+//     </div>`;
+//     const titleElement = productBox.querySelector("h3");
+//     const categoryLabelElement = productBox.querySelector("p");
+//     titleElement.innerText = item[0];
+//     categoryLabelElement.innerText = item[1];
+
+//     mainPageDisplay.appendChild(productBox);
+//     listings.push(productBox);
+// });
+
+
 }
