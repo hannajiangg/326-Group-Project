334be489121360be654d251be611d2a459f34312
maxwell3025 <amoinus@gmail.com>
2024-05-14 17:56:41 -0400

    Cleaned up JSDocs

--- src/client/api.js
+++ src/client/api.js
@@ -1,58 +1,14 @@
-// TODO make all database related functions use fetch
-// import PouchDB from "pouchdb"
-// let listingStore = new PouchDB("listing_store")
-
 import { Listing, Profile } from "./schema.js";
 export { Listing, Profile }
 
 /**
- * Returns a list of all listings.
+ * Returns a list of all listings
  * @returns { Promise<String[]> }
  */
 export async function getListings() {
   const response = await fetch('./api/listings');
   return response.json();
 }
-
-// You must specify an id when fetching a listing
-// export async function getListing() {
-//   const response = await fetch(`./api/listings`)
-//   if (response.ok) {
-//     return response.json()
-//   }
-//   else {
-//     throw new Error('Failed to fetch listings')
-//   }
-// }
-
-/**
- * Returns a listing if it exists
- * @param {string} _id 
- * @returns { Listing | null }
- */
-// export async function getListing(_id) {
-//   if(!await hasListing(_id))
-//     return null;
-//   let listing = await listingStore.get(_id);
-//   const thumbnail = await listingStore.getAttachment(_id, "thumbnail");
-
-//   const carousel = [];
-//   for(let i = 0; i < listing.carouselLength; i++){
-//     const carouselImage = await listingStore.getAttachment(_id, `carousel_${i}`);
-//     carousel.push(carouselImage);
-//   }
-//   return new Listing(
-//     listing._id, 
-//     listing.title,
-//     thumbnail,
-//     carousel,
-//     listing.cost,
-//     listing.description,
-//     listing.category,
-//     1,
-//     listing.sellerId,
-//   );
-// }
 
 /**
  * Returns a listing if it exists
@@ -111,7 +67,7 @@ export async function putListing(listing) {
 }
 
 /**
- * Deletes a given listing id;
+ * Deletes a given listing id
  * @param {string} id 
  */
 export async function deleteListing(id) {
@@ -122,7 +78,110 @@ export async function deleteListing(id) {
   if(!response.ok) throw new Error(`DELETE ./api/listings/${id} failed with error ${response.status}`);
 }
 
-export async function generateFakeData() {
+/**
+ * Returns the current user's ID
+ * @returns {Promise<string>}
+ */
+export async function getSelfProf() {
+  const response = await fetch("./api/self/profile");
+  if (!response.ok)
+    throw new Error(await response.text());
+  return response.text();
+}
+
+/**
+ * Returns true if the given id has a corresponding profile in the database
+ * @param {string} _id 
+ * @returns {Promise<boolean>}
+ */
+export async function hasProfile(_id) {
+  const response = await fetch(`./api/profiles/${_id}`);
+  return response.ok;
+}
+
+/**
+ * Fetches a profile with the given id
+ * @param {string} _id 
+ * @returns {Promise<Profile>}
+ */
+export async function getProfile(_id) {
+  const response = await fetch(`./api/profiles/${_id}`)
+  if (response.ok) {
+    return response.json();
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Returns a list of IDs corresponding to each listing sold by a given profile
+ * @param {string} id 
+ * @returns {Promise<string[]>}
+ */
+export async function getProfileListings(id) {
+  const response = await fetch(`./api/profiles/${id}/postedListings`);
+  if (response.ok) {
+    return response.json();
+  } else {
+    return [];
+  }
+}
+
+/**
+ * Adds or updates a profile in the database
+ * @param {Promise<void>} profile 
+ */
+export async function putProfile(profile) {
+  try {
+    const hasProfResponse = await fetch(`./api/profiles/${profile._id}`)
+    // temp variable to store rev
+    let rev = null
+    if (hasProfResponse.ok) {
+      const prof = await hasProfResponse.json()
+      rev = prof._rev
+    }
+    let entry = { ...profile, _rev: rev }
+    const putProfRes = await fetch(`./api/profiles`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(entry)
+    })
+
+    if (!putProfRes.ok) {
+      throw new Error(`Request to put profile failed with error ${putProfRes.status}`)
+    }
+  }
+  catch (error) {
+    console.log('Error putting profile', error)
+  }
+}
+
+/**
+ * Converts a `Blob` into a data string
+ * @param {Blob} blob 
+ * @returns {Promise<string>}
+ */
+export async function blobToURL(blob) {
+  try {
+    const dataURL = await new Promise((res, rej) => {
+      const reader = new FileReader()
+      reader.onloadend = () => res(reader.result)
+      reader.onerror = rej
+      reader.readAsDataURL(blob)
+    })
+    return dataURL
+  }
+  catch (error) {
+    console.log('Error converting blob to data url', error)
+  }
+}
+
+/**
+ * Generates fake data
+ */
+export async function generateFakeListings() {
   try {
     const imgPath = '/assets/dasweatervest.jpeg'
     const carouselImgPaths = ["/assets/sweater1.jpeg", "/assets/sweater2.jpeg", "/assets/sweater3.jpeg", "/assets/sweater1.jpeg"]
@@ -154,168 +213,9 @@ export async function generateFakeData() {
   }
 }
 
-// export async function blobToURL(blob){
-//     const reader = new FileReader();
-//     reader.readAsDataURL(blob);
-//     return await new Promise(resolve => reader.onloadend = () => {
-//         resolve(reader.result);
-//     });
-// }
-
 /**
- * Returns the current user's ID.
- * @returns {Promise<string>}
+ * Generates fake profile data
  */
-export async function getSelfProf() {
-  const response = await fetch("./api/self/profile");
-  if (!response.ok)
-    throw new Error(await response.text());
-  return response.text();
-}
-
-// export async function getSelfId(){
-//   const response = fetch("./api/profiles/self");
-//   return (await response).text();
-// }
-
-export async function blobToURL(blob) {
-  try {
-    const dataURL = await new Promise((res, rej) => {
-      const reader = new FileReader()
-      reader.onloadend = () => res(reader.result)
-      reader.onerror = rej
-      reader.readAsDataURL(blob)
-    })
-    return dataURL
-  }
-  catch (error) {
-    console.log('Error converting blob to data url', error)
-  }
-}
-
-await generateFakeData();
-
-// let profileStore = new PouchDB("profile_store");
-
-// export async function hasProfile(_id) {
-//   return await profileStore.get(_id).then(() => true, () => false)
-// }
-
-/**
- * Returns true if the given id has a corresponding profile in the database
- * @param {string} _id 
- * @returns {Promise<boolean>}
- */
-export async function hasProfile(_id) {
-  const response = await fetch(`./api/profiles/${_id}`);
-  return response.ok;
-}
-
-/**
- * Returns a profile if it exists
- * @param {string} _id 
- * @returns { Profile | null }
- */
-// export async function getProfile(_id) {
-//   if(!await hasProfile(_id))
-//     return null;
-//   let profile = await profileStore.get(_id);
-//   const pfp = await profileStore.getAttachment(_id, "pfp");
-
-// return new Profile(
-//   profile._id,
-//   pfp,
-//   profile.name,
-//   profile.email,
-//   profile.payments,
-//   profile.posted,
-//   profile.sold,
-//   profile.purchased
-// );
-// }
-
-/**
- * Fetches a profile with the given id
- * @param {string} _id 
- * @returns {Promise<Profile>}
- */
-export async function getProfile(_id) {
-  const response = await fetch(`./api/profiles/${_id}`)
-  if (response.ok) {
-    return response.json();
-  } else {
-    return null;
-  }
-}
-
-/**
- * Gets the listings being sold by a given profile.
- * @param {string} id 
- * @returns {Promise<string[]>}
- */
-export async function getProfileListings(id) {
-  const response = await fetch(`./api/profiles/${id}/postedListings`);
-  if (response.ok) {
-    return response.json();
-  } else {
-    return [];
-  }
-}
-/**
- * 
- * @param {Profile} profile 
- */
-// export async function putProfile(profile) {
-//   let entry = { ...profile };
-//   if (await hasProfile(profile._id)) {
-//     entry._rev = (await profileStore.get(profile._id))._rev
-//   }
-//   entry = { _rev: entry._rev, ...profile };
-//   delete entry.pfp;
-//   await profileStore.put(entry)
-
-//   entry = await profileStore.get(profile._id);
-//   await profileStore.putAttachment(
-//     profile._id,
-//     "pfp",
-//     entry._rev,
-//     profile.pfp,
-//     profile.pfp.type
-//   );
-// }
-
-/**
- * Uploads a new profile to the database
- * @param {Promise<void>} profile 
- */
-export async function putProfile(profile) {
-  try {
-    const hasProfResponse = await fetch(`./api/profiles/${profile._id}`)
-    // temp variable to store rev
-    let rev = null
-    if (hasProfResponse.ok) {
-      const prof = await hasProfResponse.json()
-      rev = prof._rev
-    }
-    let entry = { ...profile, _rev: rev }
-    const putProfRes = await fetch(`./api/profiles`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(entry)
-    })
-
-    if (!putProfRes.ok) {
-      throw new Error(`Request to put profile failed with error ${putProfRes.status}`)
-    }
-  }
-  catch (error) {
-    console.log('Error putting profile', error)
-  }
-}
-
-// would prolly stay the same
 export async function generateFakeProfile() {
   // await profileStore.destroy()
   // profileStore = new PouchDB("profile_store")
@@ -334,4 +234,5 @@ export async function generateFakeProfile() {
   await putProfile(fakeProfile);
 }
 
+await generateFakeListings();
 await generateFakeProfile();
